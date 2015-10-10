d13a62f8eec54fd244bcee73990bfc827715bb25
Geeker <emmanuel.aame@gmail.com>
2015-10-10 00:04:59 -0500

    Touch drag

--- Assets/Escripts/CambiarObj3d.cs
+++ Assets/Escripts/CambiarObj3d.cs
@@ -10,14 +10,18 @@ public class CambiarObj3d : MonoBehaviour {
 	public GameObject obj;
 	public MostrarDatosGPS mostrarDatos;	
 	public int indice;
-	
+
+	public GameObject tmp;
+
 	public Vector3 vr;
 	// Use this for initialization
 	void Start() {
-		objetoPadre = GameObject.Find ("Canvas");
+		//objetoPadre = GameObject.Find ("Canvas");
 		objetoCarrete = objetoPadre.GetComponent<CrearCarrete>();
 		mostrarDatos = objetoPadre.GetComponent<MostrarDatosGPS>();
 		vr = new Vector3 (1, 1, 1);
+		//tmp = new GameObject();
+		//tmp.name = "tmp";
 	}
 	
 	// Update is called once per frame
@@ -26,11 +30,16 @@ public class CambiarObj3d : MonoBehaviour {
 		//Destroy(GameObject.FindGameObjectWithTag ("objeto3d"));
 		GameObject objClon= Instantiate(objetoCarrete.objetos3d [indice], vr,objetoCentral.transform.rotation) as GameObject;
 		objetoCentral = objetoCarrete.objetos3d [indice];
+		objClon.tag = "3Dobj";
 		mostrarDatos.imagen = objetoCarrete.objetos3d [indice].name;
 		//mostrarDatos.imagen = objetoPadre.name;
 		//REVISAR OBJETO ACTUAL
-
-		objetoPadre.GetComponent<MoverRotarObjeto3d>().objeto=objClon;
+		objClon.AddComponent<D3D> ();
+		objClon.AddComponent<SeleccionObj3d> ();
+		objClon.GetComponent<SeleccionObj3d> ().moverObj3d = objetoPadre.GetComponent<MoverRotarObjeto3d> ();
+		objClon.GetComponent<SeleccionObj3d> ().objeto = objetoPadre;
+		//objetoPadre.GetComponent<MoverRotarObjeto3d>().objeto=objClon;
+		objClon.transform.SetParent (tmp.transform);
 
 	}
 
--- Assets/Escripts/SeleccionObj3d.cs
+++ Assets/Escripts/SeleccionObj3d.cs
@@ -14,7 +14,7 @@ public class SeleccionObj3d : MonoBehaviour {
 	}
 	void OnMouseUp() {
 		moverObj3d.objeto = transform.gameObject;
-		objeto.GetComponent<SeleccionDeColor>().objeto = transform.gameObject;
+//		objeto.GetComponent<SeleccionDeColor>().objeto = transform.gameObject;
 		flecha1.GetComponent<RotarX> ().objeto = transform.gameObject;
 		flecha2.GetComponent<RotarY> ().objeto = transform.gameObject;
 		flecha3.GetComponent<RotarZ> ().objeto = transform.gameObject;
